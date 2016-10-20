f6c4324dc7775dd3e2c88a8dee4c624b35759098
kenthompson2 <mark6139@yahoo.com>
2016-10-20 16:54:25 -0700

    add debug to script

--- scripts/decrypt-certificate.sh
+++ scripts/decrypt-certificate.sh
@@ -2,6 +2,11 @@
 
 # $1 - ENCRYPTION_SECRET $2 - PROFILE_NAME $3 - CERT_NAME
 
+echo "decrypting...."
+
 openssl aes-256-cbc -k "$KEY_PASSWORD" -in "scripts/profile/travis_test.mobileprovision.enc" -d -a -out "scripts/profile/travis_test.mobileprovision"
 openssl aes-256-cbc -k "$KEY_PASSWORD" -in "scripts/certs/dist.cer.enc" -d -a -out "scripts/certs/dist.cer"
 openssl aes-256-cbc -k "$KEY_PASSWORD" -in "scripts/certs/dist.p12.enc" -d -a -out "scripts/certs/dist.p12"
+
+echo `ls scripts/profile`
+echo `ls scripts/certs`
